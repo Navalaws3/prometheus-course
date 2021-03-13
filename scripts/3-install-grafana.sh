02a3ed346ebcf6055c89d4a313eb395af19044f9
Navalaws3 <77191879+Navalaws3@users.noreply.github.com>
2021-03-13 20:45:18 +0530

    updated doc for rhel installation

--- scripts/3-install-grafana.sh
+++ scripts/3-install-grafana.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-echo 'deb https://packages.grafana.com/oss/deb stable main' >> /etc/apt/sources.list
-curl https://packages.grafana.com/gpg.key | sudo apt-key add -
-sudo apt-get update
-sudo apt-get -y install grafana
+#echo 'deb https://packages.grafana.com/oss/deb stable main' >> /etc/apt/sources.list
+#curl https://packages.grafana.com/gpg.key | sudo apt-key add -
+#sudo apt-get update
+#sudo apt-get -y install grafana
+
+wget https://dl.grafana.com/oss/release/grafana-7.2.0~beta2-1.x86_64.rpm
+sudo yum install grafana-7.2.0~beta2-1.x86_64.rpm
 
 systemctl daemon-reload
 systemctl start grafana-server
